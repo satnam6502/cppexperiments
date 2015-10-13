e0987a2aabc6343dc1d72594da6baeb40e974622
Satnam Singh <satnamsingh@fb.com>
2015-10-12 19:56:01 -0700

    Initial Commit

---
+++ experiments/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  experiments
+//
+//  Created by Satnam Singh on 10/12/15.
+//  Copyright © 2015 Satnam Singh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
